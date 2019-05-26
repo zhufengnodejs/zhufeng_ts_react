600925db092a71eb03317c4c1d6150e66d87a36e
zhufengnodejs <zhufengnodejs@126.com>
2019-05-26 21:27:36 +0800

    var

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Counter from './components/Counter';
+let a=10;
 ReactDOM.render(<Counter name="计数器"/>,document.getElementById('root'));
